e7d305af9cad025964bd738b6464b919a513694f
Nicolas <nico@ip-172-31-12-229.eu-west-1.compute.internal>
2017-06-16 19:08:29 +0000

    .

--- data/data_portal.py
+++
@@ -1,6 +0,0 @@
-
-
-class DataPortal(object):
-
-	def __init__(self):
-		pass
--- utils/exceptions.py
+++
@@ -1,8 +0,0 @@
-
-
-class TradingSymbolForbidden(Exception):
-
-    msg = 'Symbols not allowed for FX trading: '
-
-    def __init__(self, symbols):
-        super(TradingSymbolForbidden, self).__init__(self.msg + str(symbols))
