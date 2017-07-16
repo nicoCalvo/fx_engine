01abf13c73db97f6df09ece685f881f07ed70c87
Nicolas <nicolas@atakama.io>
2017-07-16 19:08:56 -0300

    .

--- fxEngine/order/order_scheduler.py
+++ fxEngine/order/order_scheduler.py
@@ -1,6 +1,9 @@
 import json
-from fxEngine.utils.mb_connector import MbConnector
 import time
+from fxEngine.utils.mb_connector import MbConnector
+
+from fxEngine.utils.exceptions import RabbitConnectionError
+
 
 class Observer(object):
 
@@ -8,18 +11,16 @@ class Observer(object):
         pass
 
 
-
 class OrderScheduler(Observer):
     Q_orders = 'Q.fx_engine.filled_orders.st.'
 
-    def __init__(self, order_manager,mb_connection=None):
+    def __init__(self, order_manager, mb_connection=None):
         self.order_manager = order_manager
         self.mb_connection = mb_connection or MbConnector.get_connection()
         self.queue = self.Q_orders + order_manager._strategy.id
 
     def update(self):
         self._update_order_manager()
-
 
     def _get_orders(self):
         count = 0
@@ -39,7 +40,8 @@ class OrderScheduler(Observer):
         if count == max_count and not body:
             channel = self.mb_connection.channel()
             channel.basic_publish(
-                exchange='E_timeout_exceptions', routing_key='',body='FxEngine.OrderScheduler')
+                exchange='E_timeout_exceptions', routing_key='',
+                body='FxEngine.OrderScheduler')
             raise RabbitConnectionError()
 
             raise RabbitConnectionError('Retrieving ingest: ' + self._id)
@@ -49,11 +51,3 @@ class OrderScheduler(Observer):
         order_msg = json.loads(self._get_orders())
         filled_orders = order_msg['list_filled_orders']
         self.order_manager._update_open_orders(filled_orders)
-
-
-
-
-
-
-
-
